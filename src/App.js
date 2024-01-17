c958a3f019f69f7da18060d2610e248d69fe3c35
Rashmiraj Tiwari <rashmirajt@athmin.com>
2024-01-17 18:40:33 +0530

    Created first component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './Components/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <h1>Let's get Started</h1>
+     <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+
+    return ( 
+    <div>
+        <h3>ExpenseItem</h3>
+        <h5>Food Rs 10</h5>
+        <h5>Petrol Rs 100</h5>
+        <h5>Movies Rs 200</h5>
+    </div>
+    );
+    
+}
+
+export default ExpenseItem;
